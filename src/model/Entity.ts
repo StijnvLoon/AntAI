049ea89e62e741f233f69bfe3f92f2f843570fb4
Vincent <vincenthendriks2000@gmail.com>
2021-04-30 14:59:28 +0200

    fix conflict

--- src/model/Entity.ts
+++ src/model/Entity.ts
@@ -1,13 +1,9 @@
 import { Cell } from "./Cell";
 
 export class Entity {
+
     constructor(
         public currentCell: Cell,
-<<<<<<< HEAD
-        public name: string,
-        public movecost: number = 50
-    ) { }
-=======
         public type: EntityType
     ) {
         currentCell.entity = this
@@ -20,5 +16,4 @@ export enum EntityType {
     ENEMY,
     FOOD,
     OBSTACLE
->>>>>>> master
 }
