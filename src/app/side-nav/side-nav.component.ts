b9971dd57e9d65070c648799c112e837224d9787
Vincent <vincenthendriks2000@gmail.com>
2021-05-11 22:34:31 +0200

    side-nav functionality

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
-
+@Input ()logs
   constructor() { }
 
   ngOnInit(): void {
