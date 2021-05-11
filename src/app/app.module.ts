c11e9e11bc154f9947cd78992823a3e900e38b46
Vincent <vincenthendriks2000@gmail.com>
2021-05-12 00:25:24 +0200

    dummy docs - documentatie voorbeeld
    documentatie is voor nu erg basic, maar weergave lijkt goed te werken.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-
+import {NgxPaginationModule} from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './components/grid/grid.component';
@@ -46,7 +46,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     MatRadioModule,
     ReactiveFormsModule,
     MatSidenavModule,
-    NgxChartsModule
+    NgxChartsModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent],
