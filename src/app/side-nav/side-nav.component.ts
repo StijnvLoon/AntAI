5be57247f45df8c29b2ba70cb704f8c6a9dbbab9
StijnvLoon <stijnvanloon@home.nl>
2021-05-15 14:53:10 +0200

    anim

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -5,7 +5,7 @@ import { GlobalVars } from 'src/utils/GlobalVars';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
 
