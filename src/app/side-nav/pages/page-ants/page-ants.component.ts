a1d12c5130de0c0e48503832bffb6b510a2aa8cb
Vincent <vincenthendriks2000@gmail.com>
2021-05-16 13:33:15 +0200

    docs

--- src/app/side-nav/pages/page-ants/page-ants.component.ts
+++ src/app/side-nav/pages/page-ants/page-ants.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Cell, CellType } from 'src/model/Cell';
+import { BlockadeCell } from 'src/model/cells/BlockadeCell';
+import { FoodCell } from 'src/model/cells/FoodCell';
+import { Ant, AntType } from 'src/model/entities/Ant';
+import { CaretakerAnt } from 'src/model/entities/ants/CaretakerAnt';
+import { GathererAnt } from 'src/model/entities/ants/GathererAnt';
+import { SoldierAnt } from 'src/model/entities/ants/SoldierAnt';
+import { Colony } from 'src/model/entities/Colony';
+import { Enemy } from 'src/model/entities/Enemy';
+import { Grid } from 'src/model/Grid';
+import { GlobalVars } from 'src/utils/GlobalVars';
 
 @Component({
   selector: 'app-page-ants',
@@ -6,8 +17,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-ants.component.scss', '../pages.scss']
 })
 export class PageAntsComponent implements OnInit {
+  cell1: Cell = new Cell(0,0, CellType.EMPTY, undefined, undefined)
+  cell2: Cell = new Cell(0,0, CellType.EMPTY, undefined, undefined)
+  cell3: Cell = new Cell(0,0, CellType.EMPTY, undefined, undefined)
 
-  constructor() { }
+  globalvars = GlobalVars
+
+  constructor() {
+    this.cell1.entity = new GathererAnt(this.cell1, this.cell1, () => {});
+    this.cell2.entity = new SoldierAnt(this.cell2, this.cell2, () => {});
+    this.cell3.entity = new CaretakerAnt(this.cell3, this.cell3, () => {});
+
+   }
 
   ngOnInit(): void {
   }
